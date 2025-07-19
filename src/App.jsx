784ee7cd77a73ceac023ce6186b62ec092491184
eylommaayan <eylonmaayan@gmail.com>
2025-07-19 14:36:24 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import BookList from './BookList';
+
+export default function App() {
+  return (
+    <main className="app">
+      <h1>📚 רשימת ספרים</h1>
+      <BookList />
+    </main>
+  );
+}
---
+++ src/books.jsx
@@ -0,0 +1,23 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+export const books = [
+  {
+    author: 'למדא - האוניברסיטה הפתוחה',
+    title: 'הצבא וחברת השוק בישראל',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'אורי רוסט, שי רז',
+    title: 'השנים הראשונות של המוסד הישראלי',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'יונה גרמי בוב, אילן אביתר',
+    title: 'המוסד בטהרן',
+    img: img3,
+    id: 3,
+  },
+];
